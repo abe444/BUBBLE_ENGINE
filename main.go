ace9410a71bb8aeb6715626dce6e6e237859ff36
abe444 <spacedragon314@hotmail.com>
2024-06-24 12:46:16 -0500

    sergeant Mr.Fuckup reporting for duty sir

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/abe444/BUBBLE_ENGINE/controller"
 	"github.com/gin-gonic/gin"
 )
 
+var Db *sql.DB
+
+func Connect() {
+	var err error
+	Db, err = sql.Open("mariadb", "abe:pass1234@tcp(127.0.0.1:3306)/bubble")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err = Db.Ping(); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Successfully connected to the database.")
+}
+
 func main() {
+	Connect()
 	router := gin.Default()
 
-    router.MaxMultipartMemory = 8 << 20  // 8 MB max upload size
+	router.MaxMultipartMemory = 8 << 20 // 8 MB max upload size
 
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/app", "./app")
@@ -16,7 +34,7 @@ func main() {
 	router.Static("/static", "./static")
 	router.StaticFile("/favicon.ico", "./favicon.ico")
 
-    controller.StartupRoutes(router)
+	controller.StartupRoutes(router)
 
 	router.Run(":8080")
 }
